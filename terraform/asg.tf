7602dd2a1bf1be822237b4a068fccea52fd32c1a
af <alfomin@gmail.com>
2016-08-29 23:40:23 -0400

    Switch to Atlas data source

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -1,11 +1,12 @@
-resource "atlas_artifact" "web-ami" {
-  name = "${var.atlas_org_name}/webami"
-  type = "amazon.image"
-  version = "latest"
+data "atlas_artifact" "web" {
+    name = "${var.atlas_org_name}/webami"
+    type = "amazon.image"
+    build = "latest"
 }
+
 resource "aws_launch_configuration" "demo_launch_conf" {
     name_prefix = "demoenv-lc-"
-    image_id = "${atlas_artifact.web-ami.metadata_full.region-us-east-1}"
+    image_id = "${data.atlas_artifact.webami.metadata_full.region-us-east-1}"
     instance_type = "${var.aws_instance_type}"
 //    security_groups = [""]
     lifecycle {
